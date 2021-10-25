6b8992beb00951f922bd5c88645256418a3b44b0
Nathan Little <nthnlttl@gmail.com>
2021-10-25 15:15:58 -0400

    added more css

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_auth.serializers import UserDetailsSerializer
 
 from .models import Profile
 
@@ -6,3 +7,7 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+
+class UserDetailsSerializer(UserDetailsSerializer):
+    class meta(UserDetailsSerializer.Meta):
+        fields = ('id', 'username', 'email', 'is_active', 'is_staff', 'is_superuser',)
